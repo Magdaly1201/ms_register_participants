a3b6f43b59781727e5ce67516352924a8d728a8c
Magdaly <magdlay1201@gmail.com>
2022-05-31 21:30:30 -0300

    first commit

---
+++ src/test/java/com/magdaly/santos/meetup/participants/ParticipantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magdaly.santos.meetup.participants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParticipantsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
