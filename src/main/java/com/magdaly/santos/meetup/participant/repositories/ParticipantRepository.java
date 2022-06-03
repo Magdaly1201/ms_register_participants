5136887685e5c073ae832a37e7c0872dd81e9bef
Magdaly <magdlay1201@gmail.com>
2022-06-02 23:35:06 -0300

    add open api with swagger

--- src/main/java/com/magdaly/santos/meetup/participant/repositories/ParticipantRepository.java
+++ src/main/java/com/magdaly/santos/meetup/participant/repositories/ParticipantRepository.java
@@ -1,4 +1,10 @@
 package com.magdaly.santos.meetup.participant.repositories;
 
-public class ParticipantRepository {
+import com.magdaly.santos.meetup.participant.entities.ParticipantMeetup;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParticipantRepository extends JpaRepository<ParticipantMeetup, Integer> {
+
 }
