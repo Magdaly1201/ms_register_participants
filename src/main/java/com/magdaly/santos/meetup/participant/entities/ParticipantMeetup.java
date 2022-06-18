6c2a686a5aeeed75e2cb2b4017f5ca6055253074
Magdaly <magdlay1201@gmail.com>
2022-06-18 20:03:51 -0300

    modify imports

--- src/main/java/com/magdaly/santos/meetup/participant/entities/ParticipantMeetup.java
+++ src/main/java/com/magdaly/santos/meetup/participant/entities/ParticipantMeetup.java
@@ -1,11 +1,15 @@
 package com.magdaly.santos.meetup.participant.entities;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.time.LocalDate;
 
 @Entity
