c415c9972904c0f6c8caf2a0728dff06b63c0497
Magdaly <magdlay1201@gmail.com>
2022-08-12 00:33:01 -0300

    delete import

--- src/main/java/com/magdaly/santos/meetup/participant/controllers/ParticipantController.java
+++ src/main/java/com/magdaly/santos/meetup/participant/controllers/ParticipantController.java
@@ -1,18 +1,14 @@
 package com.magdaly.santos.meetup.participant.controllers;
 
-import com.magdaly.santos.meetup.participant.producer.SendEvent;
 import com.magdaly.santos.meetup.participant.services.ParticipantService;
 import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
