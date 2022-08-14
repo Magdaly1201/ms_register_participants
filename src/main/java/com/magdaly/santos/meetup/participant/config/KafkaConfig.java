f69d4bf3376fea3b27a8d795d98088cc044078ec
magdaly.santos <magdaly1201@gmail.com>
2022-08-14 00:39:34 -0300

    refactor add topic to variable

--- src/main/java/com/magdaly/santos/meetup/participant/config/KafkaConfig.java
+++ src/main/java/com/magdaly/santos/meetup/participant/config/KafkaConfig.java
@@ -1,6 +1,7 @@
 package com.magdaly.santos.meetup.participant.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
@@ -8,11 +9,14 @@ import org.springframework.kafka.config.TopicBuilder;
 @Configuration
 public class KafkaConfig {
 
-  @Bean
-  public NewTopic holidayEvents() {
-    return TopicBuilder.name("meetups-events")
-        .partitions(1)
-        .replicas(1)
-        .build();
-  }
+    @Value("${spring.kafka.topics.meetup-participant}")
+    private String TOPIC;
+
+    @Bean
+    public NewTopic holidayEvents() {
+        return TopicBuilder.name(TOPIC)
+                .partitions(1)
+                .replicas(1)
+                .build();
+    }
 }
