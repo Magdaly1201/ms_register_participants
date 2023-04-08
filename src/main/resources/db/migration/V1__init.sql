d7e9cb55223e0689a18a0834f5b1376f203f9670
magdaly.santos <magdaly1201@gmail.com>
2023-04-08 01:33:13 -0300

    add environments to local

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,5 +1,5 @@
 CREATE TABLE participant_meetup(
-    id BINARY(16) NOT NULL PRIMARY KEY,
+    id CHAR(36) NOT NULL PRIMARY KEY,
     username CHARACTER VARYING(255) NOT NULL,
     meet_id INTEGER NOT NULL,
     created_at timestamp not null
