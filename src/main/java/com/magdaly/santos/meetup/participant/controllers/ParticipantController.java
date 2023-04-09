e2bbd90baad2d3c0d5b432dbface1fbcb4edf500
magdaly.santos <magdaly1201@gmail.com>
2023-04-09 04:20:12 -0300

    modify structure the controller when status response

--- src/main/java/com/magdaly/santos/meetup/participant/controllers/ParticipantController.java
+++ src/main/java/com/magdaly/santos/meetup/participant/controllers/ParticipantController.java
@@ -1,15 +1,20 @@
 package com.magdaly.santos.meetup.participant.controllers;
 
+import com.magdaly.santos.meetup.participant.entities.ParticipantMeetup;
+import com.magdaly.santos.meetup.participant.exceptions.AlreadySubscribedException;
+import com.magdaly.santos.meetup.participant.exceptions.NotFoundMeetExceptions;
 import com.magdaly.santos.meetup.participant.services.ParticipantService;
 import com.sun.istack.NotNull;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/participant")
@@ -22,27 +27,30 @@ public class ParticipantController {
   }
 
   @PostMapping
-  public ResponseEntity<?> create(
+  @ResponseStatus(HttpStatus.CREATED)
+  public ParticipantMeetup create(
       @RequestParam(value = "meet_id") @NotNull int meetId,
       @RequestParam(value = "username") String username,
-      @RequestParam(value = "email") @NotNull String email) {
+      @RequestParam(value = "email") @NotNull String email) throws AlreadySubscribedException {
 
-    return new ResponseEntity<>(this.service.create(meetId, username, email), HttpStatus.CREATED);
+    return this.service.create(meetId, username, email);
   }
 
   @DeleteMapping
-  public ResponseEntity<?> delete(
+  @ResponseStatus(HttpStatus.ACCEPTED)
+  public void delete(
           @RequestParam(value = "meet_id") @NotNull int meetId,
           //TODO:User tiene que salir del autorized
           @RequestParam(value = "username") String username
-  ){
-    return new ResponseEntity<>(this.service.delete(meetId,username),HttpStatus.ACCEPTED);
+  ) throws NotFoundMeetExceptions {
+    this.service.delete(meetId,username);
   }
 
   //TODO: paso el user luego pasar el authorized
   @GetMapping
-  public ResponseEntity<?> listAll(@RequestParam(value = "username") String username){
-    return new ResponseEntity<>(this.service.findAllMeetupActive(username),HttpStatus.OK);
+  @ResponseStatus(HttpStatus.OK)
+  public List<ParticipantMeetup> listAll(@RequestParam(value = "username") String username){
+    return this.service.findAllMeetupActive(username);
   }
 
 }
